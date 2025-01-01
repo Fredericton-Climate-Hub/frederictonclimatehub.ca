78b66e47b6f3cccfcfef94fecca042e9f7bf1a0c
Patrick Schilf <pschilf@unb.ca>
2025-01-01 13:02:20 -0400

    Hero section

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,122 @@
 <template>
-  <div class="container mx-auto p-8 text-center text-4xl">
-    Hello, Fredericton Climate Hub!
+  <div class="text-center">
+    <!-- Hero -->
+    <section
+      class="section-fs h-dvh gap-y-0 py-0 font-mono text-green-100"
+      style="background: radial-gradient(rgba(255, 255, 255, 0.96) 0%, rgba(255, 255, 255, 0.96) 20%, rgba(255, 255, 255, 0.8) 100%), url('/img/climate-icons.png') 100px 100px;"
+    >
+      <div class="flex w-[85%] grow flex-col justify-center p-100">
+        <!-- Grid -->
+        <div class="grid w-full grid-cols-6 gap-25 text-100 2xs:text-150 xs:text-300 sm:grid-cols-9 md:text-400 lg:text-600">
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-500">
+            C
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-500">
+            O
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-500">
+            M
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-500">
+            M
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-500">
+            U
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-500">
+            N
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-500">
+            I
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-500">
+            T
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-500">
+            Y
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-300">
+            D
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-300">
+            R
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-300">
+            I
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-300">
+            V
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-300">
+            E
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-orange-300">
+            N
+          </span>
+          <span class="hidden h-full rounded-lg bg-metallic-900 sm:block" />
+          <span class="hidden h-full rounded-lg bg-metallic-900 sm:block" />
+          <span class="hidden h-full rounded-lg bg-metallic-900 sm:block" />
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-green-500">
+            C
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-green-500">
+            L
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-green-500">
+            I
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-green-500">
+            M
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-green-500">
+            A
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-green-500">
+            T
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-green-500">
+            E
+          </span>
+          <span class="hidden h-full rounded-lg bg-metallic-900 sm:block" />
+          <span class="hidden h-full rounded-lg bg-metallic-900 sm:block" />
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-green-300">
+            A
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-green-300">
+            C
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-green-300">
+            T
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-green-300">
+            I
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-green-300">
+            O
+          </span>
+          <span class="flex aspect-square items-center justify-center rounded-lg bg-metallic-900 p-25 leading-none text-green-300">
+            N
+          </span>
+          <span class="h-full rounded-lg bg-metallic-900" />
+          <span class="h-full rounded-lg bg-metallic-900" />
+          <span class="hidden h-full rounded-lg bg-metallic-900 sm:block" />
+        </div>
+        <div class="w-full py-50 text-start font-mono text-85 md:text-100">
+          Made in Fredericton,NB
+        </div>
+      </div>
+      <div class="grow-0">
+        <span class="text-75">We are Supported by</span>
+        <div class="w-full bg-green-100 p-150">
+          <div class="mx-auto grid w-full grid-cols-3 items-center gap-x-200 gap-y-100 sm:grid-cols-5 lg:gap-x-400 xl:w-3/4 2xl:w-2/3 2xl:gap-x-600">
+            <img src="/img/partners/climatereality.svg">
+            <img src="/img/partners/fredericton.png">
+            <img src="/img/partners/tamarack.png">
+            <img src="/img/partners/ccnb.png">
+            <img src="/img/partners/unb.png">
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
